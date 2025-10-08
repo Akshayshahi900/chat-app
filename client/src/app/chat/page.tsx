170fcdcffe5e11e5c0ce99b4a642229d61922e83
Akshayshahi900 <shahiakshay900@gmail.com>
2025-10-08 23:46:45 +0530

    working on /chat page

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
 
-const chat = () => {
-  return (
-    <div>chat</div>
-  )
+import { useState } from "react";
+import { Socket } from "socket.io-client";
+
+interface User{
+  id:String;
+  name:string;
+  username:string;
+  profilePic?:string;
+  About?:string;
+}
+interface Message{
+  id:string;
+  roomId :string;
+  senderId: string;
+  receiverId:string;
+  content:string;
+  messageType:string;
+  timestamp:Date;
+  sender:User;
 }
 
-export default chat;
+interface OnlineUser{
+  userId :string;
+  username:string;
+  isOnline:boolean;
+}
+
+export default function ChatPage() {
+  const [socket, setsocket] = useState<Socket | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [token , setToken] = useState('');
+  const [searchUsername , setSearchUsername] = useState('');
+  const [foundUsers , setReceiverId] = useState<User[]>([]);
+  
+  return (
+    <></>
+  )
+}
